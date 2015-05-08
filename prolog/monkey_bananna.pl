25ea67b4a3a7aa99ed62ba5fdd4977f2c7db3f37
nsharpe <neil.sharpe@reachlocal.com>
2015-05-07 18:47:42 -0700

    monkeybusiness

--- prolog/monkey_bananna.pl
+++ prolog/monkey_bananna.pl
@@ -5,21 +5,21 @@ move( state(middle,onbox,middle,middle,hasnot,onbox),
       grasp,
       state(middle,onbox,middle,middle,has,onbox)).
 
-move( state( P,onfloor,P,P,H,onfloor),
-      stack,
-      state( P,onfloor,P,P,H,onbox ) ).
-
-move( state(P,onfloor,P,P,H,onbox),
+move( state(middle,onfloor,middle,middle,H,onbox),
       climb,
-      state(P,onbox,P,P,H,onbox ) ).
+      state(middle,onbox,middle,middle,H,onbox ) ).
 
-move( state(P1,onfloor,P,P1,H,onfloor),
-      pushsmall(P1,P2),
-      state(P2,onfloor,P,P2,H,onfloor)).
+move( state( middle,onfloor,middle,middle,H,onfloor),
+      stack,
+      state( middle,onfloor,middle,middle,H,onbox ) ).
+
+move( state(P1,onfloor,middle,P1,H,onfloor),
+      pushsmall(P1,middle),
+      state(middle,onfloor,middle,middle,H,onfloor)).
 
 move( state(P1,onfloor,P1,P,H,onfloor),
-      pushbig(P1,P2),
-      state(P2,onfloor,P2,P,H,onfloor)).
+      pushbig(P1,middle),
+      state(middle,onfloor,middle,P,H,onfloor)).
 
 move( state(P1, onfloor, B1,B2,H,onfloor),
       walk(P1,P2),
@@ -29,15 +29,16 @@ canget( state(_,_,_,_,has,_) ).
 
 canget( State1 ) :-
 	move(State1,Move,State2),
-	canget(State2),write(Move).
+	canget(State2).
 
 canget( state(_,_,_,_,has,_), []).
 
 canget( State1, Plan ) :-
-	move(State1,Move,State2),
-	canget(State2,PartialPlan),
-	add(Move,PartialPlan,Plan).
+       move(State1,Move,State2),
+       canget(State2,PartialPlan),
+       add(Move,PartialPlan,Plan).
 
 add(X,L,[X|L]).
+
 
 creep.
