4919b58cb6c4580b269e4ab111ce9706ce59dcce
nsharpe <neil.sharpe@reachlocal.com>
2015-05-07 17:59:26 -0700

    modified monkey

--- prolog/monkey_bananna.pl
+++ prolog/monkey_bananna.pl
@@ -1,32 +1,37 @@
-% state(MonkeyHorizontal,MonkeyVertical,BigBoxPosition,SmallBoxPosition,HasBanana)
-%
+% state(MonkeyHorizontal,MonkeyVertical,BigBoxPosition,SmallBoxHorizontal,HasBanana,SmallBoxVertical)
 
-move( state(middle,onbox,middle,middle,hasnot),
+
+move( state(middle,onbox,middle,middle,hasnot,onbox),
       grasp,
-      state(middle,onbox,middle,middle,has)).
+      state(middle,onbox,middle,middle,has,onbox)).
 
-move( state(P,onfloor,P,P,H),
+move( state( P,onfloor,P,P,H,onfloor),
+      stack,
+      state( P,onfloor,P,P,H,onbox ) ).
+
+move( state(P,onfloor,P,P,H,onbox),
       climb,
-      state(P,onbox,P,P,H ) ).
+      state(P,onbox,P,P,H,onbox ) ).
 
-move( state(P1,onfloor,P1,P,H),
-      pushbig(P1,P2),
-      state(P2,onfloor,P2,P,H)).
-
-move( state(P1,onfloor,middle,P1,H),
+move( state(P1,onfloor,P,P1,H,onfloor),
       pushsmall(P1,P2),
-      state(P2,onfloor,middle,P2,H)).
+      state(P2,onfloor,P,P2,H,onfloor)).
 
-move( state(P1, onfloor, B,P,H),
+move( state(P1,onfloor,P1,P,H,onfloor),
+      pushbig(P1,P2),
+      state(P2,onfloor,P2,P,H,onfloor)).
+
+move( state(P1, onfloor, B1,B2,H,onfloor),
       walk(P1,P2),
-      state(P2,onfloor,B,P,H)).
+      state(P2,onfloor,B1,B2,H,onfloor)).
 
-canget( state(_,_,_,_,has) ) :- write("get").
+canget( state(_,_,_,_,has,_) ).
+
 canget( State1 ) :-
 	move(State1,Move,State2),
-	canget(State2).
+	canget(State2),write(Move).
 
-canget( state(_,_,_,_,has), []).
+canget( state(_,_,_,_,has,_), []).
 
 canget( State1, Plan ) :-
 	move(State1,Move,State2),
@@ -34,3 +39,5 @@ canget( State1, Plan ) :-
 	add(Move,PartialPlan,Plan).
 
 add(X,L,[X|L]).
+
+creep.
