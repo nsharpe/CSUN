5e78c0101b2a54cbeb9268ae8a8833f03838a05c
nsharpe <neil.sharpe@reachlocal.com>
2015-05-14 22:03:03 -0700

    modified move structure

--- prolog/farmer.pl
+++ prolog/farmer.pl
@@ -3,20 +3,20 @@
 
 %father in boat
 move(state(X,X,X,D,Dog,H,R),dadmovesson(X,Y),state(Y,Y,Y,D,Dog,H,R)) :- not(X=Y).
-move(state(X,X,_,X,_,_,_),dadmovesdaughter(X,Y),state(Y,Y,_,Y,_,_,_)) :- not(X=Y).
-move(state(X,X,_,_,X,_,_),dadmovesdog(X,Y),state(Y,Y,_,_,Y,_,_)) :- not(X=Y).
-move(state(X,X,_,_,_,X,_),dadmoveshamster(X,Y),state(Y,Y,_,_,_,Y,_)) :- not(X=Y).
-move(state(X,X,_,_,_,_,X),dadmovesrabbit(X,Y),state(Y,Y,_,_,_,_,Y)) :- not(X=Y).
-move(state(X,X,_,_,_,_,_),dadgoesalone(X,Y),state(Y,Y,_,_,_,_,_)) :- not(X=Y).
+move(state(X,X,S,X,D,H,R),dadmovesdaughter(X,Y),state(Y,Y,S,Y,D,H,R)) :- not(X=Y).
+move(state(X,X,S,D,X,H,R),dadmovesdog(X,Y),state(Y,Y,S,D,Y,H,R)) :- not(X=Y).
+move(state(X,X,S,D,Dog,X,R),dadmoveshamster(X,Y),state(Y,Y,S,D,Dog,Y,R)) :- not(X=Y).
+move(state(X,X,S,D,Dog,H,X),dadmovesrabbit(X,Y),state(Y,Y,S,D,Dog,H,Y)) :- not(X=Y).
+move(state(X,X,S,D,Dog,H,R),dadgoesalone(X,Y),state(Y,Y,S,D,Dog,H,R)) :- not(X=Y).
 
 
 %son in boa
-move(state(X,_,X,X,_,_,_),sonmovesdaughter(X,Y),state(Y,_,Y,Y,_,_,_)) :- not(X=Y).
-move(state(X,_,X,_,_,X,_),sonmoveshamsters(X,Y),state(Y,_,Y,_,_,Y,_)) :- not(X=Y).
-move(state(X,_,X,_,_,_,_),songoesalone(X,Y),state(Y,_,Y,_,_,_,_)) :- not(X=Y).
+move(state(X,F,X,X,Dog,H,R),sonmovesdaughter(X,Y),state(Y,F,Y,Y,Dog,H,R)) :- not(X=Y).
+move(state(X,F,X,D,Dog,X,R),sonmoveshamsters(X,Y),state(Y,F,Y,D,Dog,Y,R)) :- not(X=Y).
+move(state(X,F,X,D,Dog,H,R),songoesalone(X,Y),state(Y,F,Y,D,Dog,H,R)) :- not(X=Y).
 %daughter in boat
-move(state(X,_,_,X,_,_,X),daughtermovesrabbits(X,Y),state(Y,_,_,Y,_,_,Y)) :- not(X=Y).
-move(state(X,_,_,X,_,_,_),daughtergoesalone(X,Y),state(Y,_,_,Y,_,_,_)) :- not(X=Y).
+move(state(X,F,S,X,Dog,H,X),daughtermovesrabbits(X,Y),state(Y,F,S,Y,Dog,H,Y)) :- not(X=Y).
+move(state(X,F,S,X,Dog,H,R),daughtergoesalone(X,Y),state(Y,F,S,Y,Dog,H,R)) :- not(X=Y).
 
 
 dogdoesnotbite(state(_,X,_,_,X,_,_)).
@@ -37,8 +37,9 @@ start(State,Plan):-getplan(State,Plan,[]).
 done(state(w,w,w,w,w,w,w)).
 
 
-getplan(State,_,_) :- done(State),!.
+getplan(State,_,_) :- done(State).
 getplan(State1,Plan,PastStates) :- move(State1,Move,State2),
+			write(State2),
 			canmove(State2,PastStates),
 			add(State2,PastStates,NewStates),
 			getplan(State2,PartialPlan,NewStates),
